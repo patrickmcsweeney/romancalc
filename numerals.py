b34ea5c4bfaec29768c63c73da01a805896b36cb
Jonathan Scott <jonathan@jscott.me>
2014-07-10 19:55:20 +0100

    adding works

--- numerals.py
+++ numerals.py
@@ -1,10 +1,46 @@
+replace_table = {
+    "NI": "I",
+    "IIII": "IV",
+    "IVI": "V",
+    "VIV": "IX",
+    "IXI": "X",
+}
+
 class romannumeral:
-	def __init__(val):
-		self.val = val
+    def __init__(self, val):
+        self.val = val
+
+    def increment(self):
+        newval = self.val + "I"
+        for rep, wi in replace_table.items():
+            newval = newval.replace(rep, wi)
+        return romannumeral(newval)
+
+    def decrement(self):
+        if self.val == "I":
+            return romannumeral("N")
+        newnum = romannumeral("I")
+        lastnum = romannumeral("N")
+        while newnum.val != self.val:
+            newnum = newnum.increment()
+            lastnum = lastnum.increment()
+        return lastnum
+
+    def __add__(self, numtoadd):
+        ret = romannumeral(self.val)      
+        while numtoadd.val != "N":
+            ret = ret.increment()
+            numtoadd = numtoadd.decrement()
+        return ret
+
+    def __str__(self):
+        return self.val
 
 
-I = romannumeral("i")
+v = romannumeral("X")
+for i in range(5):
+    v = v.decrement()
+    print v
 
-v = I
-v++
-print v
+foo = romannumeral("V") + romannumeral("IV")
+print foo
